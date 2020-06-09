f4ba8b2e35062a0e2c9049f871bf1cac5ed92f95
Iago Lucas <iago0905@hotmail.com>
2020-06-09 17:39:56 -0300

    imple Create form

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -20,7 +20,11 @@ namespace SalesWebMvc.Services
         {
             return _context.Seller.ToList();
         }
-
+        public void insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
 
     }
 }
